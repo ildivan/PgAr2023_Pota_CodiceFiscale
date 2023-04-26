e23a5fb75e3ffd2eb1e07dda4868db8806f44c4a
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-04-26 18:53:42 +0200

    Update InvalidInputException.java
    added javadoc

--- CodiceFiscale/src/main/java/CodiceFiscale/error/InvalidInputException.java
+++ CodiceFiscale/src/main/java/CodiceFiscale/error/InvalidInputException.java
@@ -1,8 +1,11 @@
 package CodiceFiscale.error;
 
+/**
+ * Exception thrown in case of invalid input in fiscalcode and person packages.
+ */
 public class InvalidInputException extends RuntimeException{
     public InvalidInputException() {
-        this("Name not valid.");
+        this("Input not valid.");
     }
 
     public InvalidInputException(String message) {
